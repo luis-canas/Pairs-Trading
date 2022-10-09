fc5b26b46a9c6964f3d03883569f98d81bc3f0bd
luis-anton <luis.antonio.canas5@gmail.com>
2022-10-09 20:00:19 +0100

    add cointegration

---
+++ class_Trader.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+
+class Trader:
+
+
+    def __init__(self):
+        self.fee=0
+        self.trade_threshold=0
+        pass
+ 
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from class_Pairs import Pairs
+from class_Trader import Trader
+
+import numpy as np
+
+from datetime import datetime
+
+start = datetime(2013, 1, 1)
+end = datetime(2018, 1, 1)
+tickers = ['AAPL', 'ADBE', 'ORCL', 'EBAY', 'MSFT', 'QCOM', 'HPQ', 'JNPR', 'AMD', 'IBM', 'SPY']
 
 
-class pairs:
-
-    pass
+# Test Tickers
+cointegration=Pairs(start,end,tickers)
+selected_pairs=cointegration.get_pairs()
+print(selected_pairs)
