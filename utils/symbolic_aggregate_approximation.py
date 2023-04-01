71a6edcbb98ba7d6c403f43aee6e26e4227b5aaa
luis-anton <luis.antonio.canas5@gmail.com>
2023-04-01 17:24:14 +0100

    update

--- utils/symbolic_aggregate_approximation.py
+++ utils/symbolic_aggregate_approximation.py
@@ -205,13 +205,14 @@ duration = 5  # seconds
 freq = 1  # Hz
 
 # Create a time vector
-t = np.linspace(0, duration, num=1000)
+t = np.linspace(0, duration, num=5000)
 
 # Create an amplitude vector that increases linearly with time
-amplitude = np.linspace(0, 1, num=1000)
+amplitude = np.linspace(0, 1, num=5000)
 
 # Generate the sine wave
 y = amplitude * np.sin(2 * np.pi * freq * t)
+y1 = np.sin(2 * np.pi * freq * t)
 
 # Plot the waveform
 plt.plot(t, y)
@@ -223,20 +224,21 @@ plt.show()
 ts1=np.random.normal(0,1,10)
 ts1=y
 ts2=np.random.normal(0,1,10)
+ts2=y1
 
 
 print(ts1,len(ts1))
 # print(ts2,len(ts2))
 
 
-symbols1,ind=timeseries2symbol(ts1,500,10,10)
+symbols1,ind=timeseries2symbol(ts1,1000,5,10)
 print(symbols1,ind)
 symbols12=convert_symbols(symbols1)
 
 
-symbols2,_=timeseries2symbol(ts2,5,2,10)
+symbols2,_=timeseries2symbol(ts2,1000,2,10)
 symbols22=convert_symbols(symbols2)
-# print(symbols2,len(symbols2))
+# print(symbols2,ind)
 
 # dist=min_dist(symbols1[0],symbols2[0],10,1)
 
